b5e7c354343efe0b8ff66152b2a2b248c682ca28
siyam <galib.abdullah04@gmail.com>
2020-07-14 13:43:07 +0600

    route changed

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -11,8 +11,8 @@ const auth_middleware = require('../middlewares/auth')
 
 /* routes */
 router.post('/product', auth_middleware.Auth, productController.product)
-router.get('/product', auth_middleware.Auth, productController.product)
-router.get('/product/:id', auth_middleware.Auth, productController.product)
+// router.get('/product', auth_middleware.Auth, productController.product)
+router.get('/product/:id', productController.product)
 router.put('/product/:id', auth_middleware.Auth, productController.updateProduct) // Auth = Middleware
 router.delete('/product/:id', auth_middleware.Auth, productController.deleteProduct)
 
