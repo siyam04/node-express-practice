207d55381d8cb3353fc8e8a8c9260dbcddec56fe
siyam <galib.abdullah04@gmail.com>
2020-07-14 12:35:57 +0600

    Added a Procfile.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cartRoute = require('./routes/cartRouter')
 
 /*====================================SYSTEM INTEGRATION=============================*/
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 /*====================================USE PACKAGES===================================*/
 app.use(bodyParser.json())
