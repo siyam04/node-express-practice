20d87480f496e3050c4ee60600a9b3a2404f2605
siyam <galib.abdullah04@gmail.com>
2020-07-21 12:09:23 +0600

    express-validator code cleaned

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,9 @@
 /* importing express */
 const express = require('express')
+const {body} = require('express-validator')
 
 /* creating router */
 const router = express.Router()
-const { body} = require('express-validator');
 
 /* importing controllers */
 const productController = require('../controllers/productController')
@@ -11,29 +11,30 @@ const auth_middleware = require('../middlewares/auth')
 
 
 /* routes */
-router.post('/product',
-[
+// express-validator added
+router.post('/product', [
     body('name').isString(),
     body('category').isString(),
     body('price').isFloat(),
     body('quantity').isNumeric(),
     body('description').isString()
-],
-productController.product)
+], productController.product)
 
 router.get('/product', productController.product)
+
 router.get('/product/:id', productController.product)
-router.put('/product/:id', 
-[
+
+// express-validator added
+router.put('/product/:id', [
     body('name').isString(),
     body('category').isString(),
     body('price').isFloat(),
     body('quantity').isNumeric(),
     body('description').isString()
-],
-productController.updateProduct)
+], productController.updateProduct)
 
 router.delete('/product/:id', auth_middleware.Auth, productController.deleteProduct)
+
 
 /* exporting routes */
 module.exports = router
