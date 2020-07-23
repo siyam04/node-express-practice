e6d2d619741372a7288d39205ee73a8dfbbcb6ed
siyam <galib.abdullah04@gmail.com>
2020-07-23 12:28:36 +0600

    express-validator updated

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,34 +1,29 @@
 /*==================================== IMPORTING =============================*/
 // packages
 const express = require('express')
-const {body} = require('express-validator')
 
 // custom controllers
 const authController = require('../controllers/authController')
+
+// custom validators
+const authValidation = require('../validators/authValidation')
 
 /*==================================== CREATING ROUTER =============================*/
 const router = express.Router()
 
 /*==================================== ROUTES =============================*/
 // 1. Registration (POST) // express-validator added
-router.post('/register', [
-    body('username').isAlphanumeric().withMessage('must be alphaNumeric'),
-    body('password').isLength({min: 8}).withMessage('must be at least 8 characters long'),
-    body('email').isEmail().withMessage('must be a valid email address'),
-    body('firstName').isString().withMessage('must be string'),
-    body('firstName').isString().withMessage('must be string'),
-], authController.register)
+router.route('/register').post(authValidation.registrationValidation, authController.register)
 
 // 2. Login (POST) // express-validator added
-router.post('/login', [
-    body('email').isEmail().withMessage('must be a valid email address'),
-    body('password').isLength({min: 8}).withMessage('must be at least 8 characters long'),
-], authController.login)
+router.route('/login').post(authValidation.loginValidation, authController.login)
 
 // router.post('/logout', authController.logout)
 
 /*==================================== EXPORTING ROUTES =============================*/
 module.exports = router
+
+
 
 
 
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,25 +1,20 @@
 /*==================================== IMPORTING =============================*/
 // packages
 const express = require('express')
-const {body} = require('express-validator')
 
 // custom controllers
 const productController = require('../controllers/productController')
 const auth_middleware = require('../middlewares/auth')
+
+// custom validators
+const productValidation = require('../validators/productValidation')
 
 /*==================================== CREATING ROUTER =============================*/
 const router = express.Router()
 
 /*==================================== ROUTES =============================*/
 // 3. Create Product (POST) // express-validator added
-router.post('/product', [
-    body('name').isString().withMessage('must be string'),
-    body('category').isString().withMessage('must be string'),
-    body('price').isFloat().withMessage('must be float'),
-    body('quantity').isNumeric().withMessage('must be numeric'),
-    body('description').isString().withMessage('must be string'),
-    body('imageUrl').isBase64().withMessage('must be base64')
-], productController.product)
+router.route('/product').post(productValidation.createProductValidation, productController.product)
 
 // 4. Product List (GET)
 router.get('/product', productController.product)
@@ -28,14 +23,7 @@ router.get('/product', productController.product)
 router.get('/product/:id', productController.product)
 
 // 6. Edit Product (PUT) // express-validator added
-router.put('/product/:id', [
-    body('name').isString().withMessage('must be string'),
-    body('category').isString().withMessage('must be string'),
-    body('price').isFloat().withMessage('must be float'),
-    body('quantity').isNumeric().withMessage('must be numeric'),
-    body('description').isString().withMessage('must be string'),
-    body('imageUrl').isBase64().withMessage('must be base64')
-], productController.updateProduct)
+router.route('/product/:id').put(productValidation.editProductValidation, productController.updateProduct)
 
 // 7. Delete Product (DELETE)
 router.delete('/product/:id', auth_middleware.Auth, productController.deleteProduct)
